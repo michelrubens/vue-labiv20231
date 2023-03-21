4ae214d9877a7a54c618217f646a626f501a173c
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-03-21 22:59:37 +0000

    Axios

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://8080-mineda-springlabiv20231-861r9pte7ev.ws-us90.gitpod.io/';
 
 import './assets/main.css'
 
